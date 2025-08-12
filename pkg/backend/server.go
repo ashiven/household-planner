c5e1bea3b4a43cd67ac32391cd0c2b6f25baae9f
ashiven <nevisha@pm.me>
2025-08-12 18:22:04 +0200

    fix cors

--- pkg/backend/server.go
+++ pkg/backend/server.go
@@ -12,19 +12,32 @@ import (
 func StartServer() {
 	fmt.Println("[INFO] Starting Household Planner API server...")
 	router := mux.NewRouter()
-	router.HandleFunc("/members", getMembers).Methods("GET")
-	router.HandleFunc("/members", updateMembers).Methods("POST")
+	router.Use(mux.CORSMethodMiddleware(router))
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			if r.Method == "OPTIONS" {
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	})
 
-	router.HandleFunc("/tasks/daily", getDailyTasks).Methods("GET")
-	router.HandleFunc("/tasks/daily", updateDailyTasks).Methods("POST")
+	router.HandleFunc("/members", getMembers).Methods("GET", "OPTIONS")
+	router.HandleFunc("/members", updateMembers).Methods("POST", "OPTIONS")
 
-	router.HandleFunc("/tasks/weekly", getWeeklyTasks).Methods("GET")
-	router.HandleFunc("/tasks/weekly", updateWeeklyTasks).Methods("POST")
+	router.HandleFunc("/tasks/daily", getDailyTasks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/tasks/daily", updateDailyTasks).Methods("POST", "OPTIONS")
 
-	router.HandleFunc("/tasks/monthly", getMonthlyTasks).Methods("GET")
-	router.HandleFunc("/tasks/monthly", updateMonthlyTasks).Methods("POST")
+	router.HandleFunc("/tasks/weekly", getWeeklyTasks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/tasks/weekly", updateWeeklyTasks).Methods("POST", "OPTIONS")
 
-	router.HandleFunc("/auth", checkAdminPassword).Methods("POST")
+	router.HandleFunc("/tasks/monthly", getMonthlyTasks).Methods("GET", "OPTIONS")
+	router.HandleFunc("/tasks/monthly", updateMonthlyTasks).Methods("POST", "OPTIONS")
+
+	router.HandleFunc("/auth", checkAdminPassword).Methods("POST", "OPTIONS")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
